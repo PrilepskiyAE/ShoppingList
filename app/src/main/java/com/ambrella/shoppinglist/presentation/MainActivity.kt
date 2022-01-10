19169c3ea61e6ea1fa430136b0bccf527dcfd986
Prilepskiy_AE <woolf7754286@yandex.ru>
2022-01-10 23:13:00 +0700

    Test1 repository

--- app/src/main/java/com/ambrella/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/ambrella/shoppinglist/presentation/MainActivity.kt
@@ -2,11 +2,26 @@ package com.ambrella.shoppinglist.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.lifecycle.ViewModelProvider
 import com.ambrella.shoppinglist.R
+import com.ambrella.shoppinglist.domain.Shopitem
+import com.ambrella.shoppinglist.presentation.ViewModel.ShopViewModel
+import com.ambrella.shoppinglist.presentation.ViewModel.ShopViewModelFactory
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var viewModel: ShopViewModel
+    private lateinit var viewModelFactory: ViewModelProvider.Factory
+    private lateinit var bt:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bt=findViewById(R.id.button)
+        viewModelFactory=ShopViewModelFactory(this)
+        viewModel=ViewModelProvider(this,viewModelFactory).get(ShopViewModel::class.java)
+        bt.setOnClickListener {
+            viewModel.addShopItem(Shopitem("test",5,true))
+        }
+
     }
 }
