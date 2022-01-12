ff7dfd8534e587442038c99fe73879b767e07261
Prilepskiy_AE <woolf7754286@yandex.ru>
2022-01-12 22:54:59 +0700

    Create DI

---
+++ app/src/main/java/com/ambrella/shoppinglist/presentation/adapter/ShopAdapter.kt
@@ -0,0 +1,23 @@
+package com.ambrella.shoppinglist.presentation.adapter
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ShopAdapter : RecyclerView.Adapter<ShopViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShopViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ShopViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+}
+class ShopViewHolder(view: View):RecyclerView.ViewHolder(view) {
+
+}
