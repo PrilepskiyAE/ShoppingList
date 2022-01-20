64653f827ea4e78e1f34c4dc917fb7a3a1d8c67f
Prilepskiy_AE <woolf7754286@yandex.ru>
2022-01-20 22:03:16 +0700

    bindig add

--- app/src/main/java/com/ambrella/shoppinglist/presentation/adapter/ShopAdapter.kt
+++ app/src/main/java/com/ambrella/shoppinglist/presentation/adapter/ShopAdapter.kt
@@ -1,23 +1,43 @@
 package com.ambrella.shoppinglist.presentation.adapter
 
+import android.view.LayoutInflater
+import android.view.TextureView
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.ambrella.shoppinglist.R
+import com.ambrella.shoppinglist.domain.Shopitem
 
 class ShopAdapter : RecyclerView.Adapter<ShopViewHolder>() {
+
+    var shoplist= listOf<Shopitem>()
+    set(value){
+        field=value
+        notifyDataSetChanged()
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShopViewHolder {
-        TODO("Not yet implemented")
+        val view=LayoutInflater.from(parent.context).inflate(R.layout.item_shop,parent,false)
+        return ShopViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ShopViewHolder, position: Int) {
-        TODO("Not yet implemented")
+       val shopitem=shoplist[position]
+        holder.tvName.text=shopitem.name
+        holder.tvCount.text=shopitem.count.toString()
+        holder.itemView.setOnLongClickListener {
+            true
+        }
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount(): Int =shoplist.size
+
 
 }
 class ShopViewHolder(view: View):RecyclerView.ViewHolder(view) {
+    val tvName=view.findViewById<TextView>(R.id.tvTitle)
+    val tvCount=view.findViewById<TextView>(R.id.tvCount)
+
 
 }
